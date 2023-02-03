2c6e5265829edee2cd4ab58a0e584f925678d5b8
Tobi Olutimehin <oluwatobi@nextdoor.com>
2023-02-03 15:30:19 -0500

    example

--- feature/trips/src/androidTest/java/com/planner/feature/trips/ExampleInstrumentedTest.kt
+++ feature/trips/src/androidTest/java/com/planner/feature/trips/ExampleInstrumentedTest.kt
@@ -1,8 +1,10 @@
 package com.planner.feature.trips
 
+import androidx.appcompat.app.AppCompatActivity
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -13,6 +15,9 @@ import org.junit.runner.RunWith
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    val activityTestRule = ActivityScenarioRule(AppCompatActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
