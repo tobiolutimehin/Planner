bec1ca1be6fdf30b32d154e1b05c268856384d55
Tobi Olutimehin <87507894+tobiolutimehin@users.noreply.github.com>
2023-02-03 15:37:36 -0500

    Tasks add task UI (#45)
    * Tasks Room Work
    
    * Tasks View Model
    
    * Add task ui

---
+++ feature/tasks/src/main/java/com/planner/feature/tasks/adapter/TasksRecyclerViewAdapter.kt
@@ -0,0 +1,56 @@
+package com.planner.feature.tasks.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.planner.core.data.entity.Task
+import com.planner.feature.tasks.databinding.TaskItemBinding
+
+class TasksRecyclerViewAdapter(val removeTask: (Task) -> Unit) :
+    ListAdapter<Task, TasksRecyclerViewAdapter.ViewHolder>(DiffCallback) {
+
+    class ViewHolder(
+        private var binding: TaskItemBinding,
+        val removeTask: (Task) -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(task: Task) {
+            binding.apply {
+                task.contributor?.let {
+                    contributorName.isVisible = true
+                    contributorName.text = task.contributor
+                }
+                taskListItem.text = task.description
+                button.setOnClickListener {
+                    removeTask(task)
+                }
+            }
+        }
+    }
+
+    companion object DiffCallback : DiffUtil.ItemCallback<Task>() {
+        override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean =
+            oldItem.hashCode() == newItem.hashCode()
+
+        override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean =
+            oldItem == newItem
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            TaskItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            ),
+            removeTask
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val task = getItem(position)
+        holder.bind(task)
+    }
+}
