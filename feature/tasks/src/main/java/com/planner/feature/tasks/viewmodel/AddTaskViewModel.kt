4e46eaa6983f1c50c15848fb0e7f7150f6fa5386
Tobi Olutimehin <87507894+tobiolutimehin@users.noreply.github.com>
2023-02-03 15:36:47 -0500

    Tasks view model (#44)
    * Tasks Room Work
    
    * Tasks View Model

---
+++ feature/tasks/src/main/java/com/planner/feature/tasks/viewmodel/AddTaskViewModel.kt
@@ -0,0 +1,32 @@
+package com.planner.feature.tasks.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.planner.core.data.entity.Task
+import com.planner.core.data.entity.TaskManagerType
+
+class AddTaskViewModel : ViewModel() {
+
+    private var _taskList = MutableLiveData<List<Task>>()
+    val taskList: LiveData<List<Task>> = _taskList
+
+    private var _taskManagementType = MutableLiveData<TaskManagerType>()
+    val taskManagerType: LiveData<TaskManagerType> = _taskManagementType
+
+    fun addTask(task: Task) {
+        val updatedTaskList = _taskList.value?.toMutableList() ?: mutableListOf()
+        updatedTaskList.add(task)
+        _taskList.value = updatedTaskList
+    }
+
+    fun removeTask(task: Task) {
+        val updatedTaskList = _taskList.value?.toMutableList() ?: mutableListOf()
+        updatedTaskList.remove(task)
+        _taskList.value = updatedTaskList
+    }
+
+    fun setTaskManagementType(taskManagerType: TaskManagerType) {
+        _taskManagementType.value = taskManagerType
+    }
+}
