25df7dcb8401d689f6339133ad060bae46b19af0
Tobi Olutimehin <oluwatobi@nextdoor.com>
2023-02-18 22:10:16 -0500

    Detail Fragment stuff

--- feature/tasks/src/main/java/com/planner/feature/tasks/fragment/TaskManagerDetailFragment.kt
+++ feature/tasks/src/main/java/com/planner/feature/tasks/fragment/TaskManagerDetailFragment.kt
@@ -5,57 +5,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.planner.feature.tasks.R
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.navArgs
+import com.planner.core.ui.BaseApplication
+import com.planner.feature.tasks.databinding.FragmentTaskManagerDetailBinding
+import com.planner.feature.tasks.viewmodel.TasksViewModel
+import com.planner.feature.tasks.viewmodel.TasksViewModelFactory
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [TaskManagerDetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TaskManagerDetailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private val arguments: TaskManagerDetailFragmentArgs by navArgs()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private var _binding: FragmentTaskManagerDetailBinding? = null
+    private val binding = _binding!!
+
+    private val tasksViewModel: TasksViewModel by activityViewModels {
+        TasksViewModelFactory(((activity?.application as BaseApplication).database).taskManagerDao())
     }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_task_manager_detail, container, false)
+    ): View {
+        _binding = FragmentTaskManagerDetailBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TaskManagerDetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TaskManagerDetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val taskManagerId = arguments.taskManagerId
+        tasksViewModel.getTaskManager(taskManagerId)
     }
 }
