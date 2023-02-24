a5b2146c649d8fb1231587c1261003af56238b1b
Tobi Olutimehin <oluwatobi@nextdoor.com>
2023-02-24 15:15:18 -0500

    Add the bottom navigation

--- app/src/main/java/com/planner/MainActivity.kt
+++ app/src/main/java/com/planner/MainActivity.kt
@@ -2,10 +2,13 @@ package com.planner
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.navigation.NavController
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.planner.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,16 +22,54 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
+        appBarConfiguration = AppBarConfiguration
+            .Builder(
+                com.planner.feature.tasks.R.id.taskManagerListFragment,
+                com.planner.feature.trips.R.id.listTripFragment,
+            )
+            .build()
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
-        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        showBottomNav()
+        setUpBottomNav()
         setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return super.onSupportNavigateUp() || navController.navigateUp()
+    }
+
+    private fun setUpBottomNav() {
+        with(binding) {
+            setSupportActionBar(this@with.toolbar)
+            this@with.bottomMenu.setupWithNavController(navController)
+            this@with.bottomMenu.setOnItemSelectedListener { item ->
+                val options = NavOptions.Builder()
+                    .setPopUpTo(
+                        R.id.nav_graph,
+                        true,
+                    ) // Set the start destination of your graph here
+                    .build()
+                navController.navigate(item.itemId, null, options)
+                true
+            }
+        }
+    }
+
+    private fun showBottomNav() {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                com.planner.feature.trips.R.id.listTripFragment, com.planner.feature.tasks.R.id.taskManagerListFragment -> {
+                    binding.bottomMenu.isVisible = true
+                }
+
+                else -> {
+                    binding.bottomMenu.isVisible = false
+                }
+            }
+        }
     }
 }
