f74c151e4c2434322f066529e1cccdd6948d2da8
Tobi Olutimehin <oluwatobi@nextdoor.com>
2023-03-10 14:07:57 -0500

    Adding the home page ui

--- app/src/main/java/com/planner/fragment/HomeFragment.kt
+++ app/src/main/java/com/planner/fragment/HomeFragment.kt
@@ -5,12 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import com.planner.core.ui.BaseApplication
 import com.planner.databinding.FragmentHomeBinding
+import com.planner.feature.tasks.adapter.TaskManagerListAdapter
+import com.planner.feature.tasks.viewmodel.TasksViewModel
+import com.planner.feature.tasks.viewmodel.TasksViewModelFactory
+import com.planner.feature.trips.viewmodel.TripsViewModel
+import com.planner.feature.trips.viewmodel.TripsViewModelFactory
+import java.util.Date
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    private val tasksViewModel: TasksViewModel by activityViewModels {
+        TasksViewModelFactory(((activity?.application as BaseApplication).database).taskManagerDao())
+    }
+
+    private val tripViewModel: TripsViewModel by activityViewModels {
+        TripsViewModelFactory(((activity?.application as BaseApplication).database).tripDao())
+    }
+
+    private lateinit var adapter: TaskManagerListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,6 +44,36 @@ class HomeFragment : Fragment() {
         binding.fab.setOnClickListener {
             val action = HomeFragmentDirections.actionHomeFragmentToPlanListDialogFragment()
             findNavController().navigate(action)
+        }
+
+        adapter = TaskManagerListAdapter(
+            context = context,
+            openDetail = {},
+        )
+        binding.tasksRecyclerView.adapter = adapter
+
+        tasksViewModel.tasks.observe(viewLifecycleOwner) { managerWithTasks ->
+            val pending = managerWithTasks.filter {
+                it.tasks.any { !it.isDone }
+            }
+            adapter.submitList(
+                pending,
+            )
+            binding.pendingTasks.text = resources.getQuantityString(
+                com.planner.core.ui.R.plurals.pending_x_tasks,
+                pending.size,
+                pending.size,
+            )
+        }
+
+        tripViewModel.trips.observe(viewLifecycleOwner) { trips ->
+            val toGo = trips.filter { it.departureTime > Date().time }.size
+
+            binding.planningTrips.text = resources.getQuantityString(
+                com.planner.core.ui.R.plurals.planning_x_trips,
+                toGo,
+                toGo,
+            )
         }
     }
 }
