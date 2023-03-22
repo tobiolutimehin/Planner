bbe5c569dd7fa98f0ff9445621b580a9edd38c8f
Tobi Olutimehin <oluwatobi@nextdoor.com>
2023-03-22 01:31:05 -0400

    kdocs for core ui

--- core/ui/src/main/java/com/planner/core/ui/BaseApplication.kt
+++ core/ui/src/main/java/com/planner/core/ui/BaseApplication.kt
@@ -3,6 +3,13 @@ package com.planner.core.ui
 import android.app.Application
 import com.planner.core.data.database.PlannerDatabase
 
+/**
+ * The base application class for the Planner app.
+ */
 class BaseApplication : Application() {
+
+    /**
+     * The instance of the [PlannerDatabase].
+     */
     val database by lazy { PlannerDatabase.getDatabase(context = this@BaseApplication) }
 }
