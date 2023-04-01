7496b3d9dd6730dd9951b6d0a55d69b78240ae99
Tobi Olutimehin <oluwatobi@nextdoor.com>
2023-03-31 21:46:20 -0400

    navving

--- feature/trips/src/main/java/com/planner/feature/trips/fragment/ListTripFragment.kt
+++ feature/trips/src/main/java/com/planner/feature/trips/fragment/ListTripFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -75,18 +74,5 @@ class ListTripFragment : Fragment() {
     override fun onDestroy() {
         _binding = null
         super.onDestroy()
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        activity?.onBackPressedDispatcher?.addCallback(
-            this,
-            object : OnBackPressedCallback(true) {
-                override fun handleOnBackPressed() {
-                    activity?.finish()
-                }
-            },
-        )
     }
 }
