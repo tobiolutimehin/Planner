e00cec65bc3ffab3a31659f51a5eeac936bc72ba
tobiolutimehin <olutimehintobi@gmail.com>
2023-12-28 11:54:37 -0500

    remove database from application

--- app/src/main/java/com/planner/BaseApplication.kt
+++ app/src/main/java/com/planner/BaseApplication.kt
@@ -1,17 +1,10 @@
 package com.planner
 
 import android.app.Application
-import com.planner.core.data.database.PlannerDatabase
 import dagger.hilt.android.HiltAndroidApp
 
 /**
  * The base application class for the Planner app.
  */
 @HiltAndroidApp
-class BaseApplication : Application() {
-
-    /**
-     * The instance of the [PlannerDatabase].
-     */
-    val database by lazy { PlannerDatabase.getDatabase(context = this@BaseApplication) }
-}
+class BaseApplication : Application()
