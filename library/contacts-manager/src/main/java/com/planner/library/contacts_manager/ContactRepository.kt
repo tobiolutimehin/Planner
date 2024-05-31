f8d6ee80783c23b0f9458eaa28da3c025ca5ddf8
Tobi Olutimehin <olutimehintobi@gmail.com>
2024-05-31 01:07:37 -0400

    contact repository

--- library/contacts-manager/src/main/java/com/planner/library/contacts_manager/ContactRepository.kt
+++ library/contacts-manager/src/main/java/com/planner/library/contacts_manager/ContactRepository.kt
@@ -14,11 +14,13 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Inject
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.withContext
 import javax.inject.Singleton
 
 data class Contact(
-    val id: Int,
+    val id: Long,
     val name: String,
     val phone: String,
 )
@@ -38,89 +40,107 @@ interface ContactFetcher {
 /**
  * The implementation of the [ContactFetcher].
  */
-class ContactFetcherImpl
-    @Inject
-    constructor(
-        private val context: Context,
-    ) : ContactFetcher {
-        override suspend fun fetchContactList(): Set<Contact> {
-            val contactList = mutableListOf<Contact>()
-            if (ContextCompat.checkSelfPermission(
-                    context,
-                    Manifest.permission.READ_CONTACTS,
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                context.activity()?.let {
-                    ActivityCompat.requestPermissions(
-                        it,
-                        arrayOf(Manifest.permission.READ_CONTACTS),
-                        0,
-                    )
-                }
-            } else {
-                try {
-                    val contentResolver = context.contentResolver
-                    val uri = ContactsContract.Contacts.CONTENT_URI
-                    val cursor =
-                        contentResolver.query(
-                            uri,
-                            null,
-                            null,
-                            null,
-                            ContactsContract.Contacts.DISPLAY_NAME + " ASC",
+class ContactFetcherImpl(
+    private val context: Context,
+) : ContactFetcher {
+    private val cacheMutex = Mutex()
+
+    private val cache: MutableSet<Contact> = mutableSetOf()
+
+    override suspend fun fetchContactList(): Set<Contact> {
+        cacheMutex.lock()
+
+        try {
+            if (cache.isNotEmpty()) {
+                return cache
+            }
+            return withContext(Dispatchers.IO) {
+                val contactList = mutableListOf<Contact>()
+                if (ContextCompat.checkSelfPermission(
+                        context,
+                        Manifest.permission.READ_CONTACTS,
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    context.activity()?.let {
+                        ActivityCompat.requestPermissions(
+                            it,
+                            arrayOf(Manifest.permission.READ_CONTACTS),
+                            0,
                         )
+                    }
+                } else {
+                    try {
+                        val contentResolver = context.contentResolver
+                        val uri = ContactsContract.Contacts.CONTENT_URI
+                        val cursor =
+                            contentResolver.query(
+                                uri,
+                                null,
+                                null,
+                                null,
+                                ContactsContract.Contacts.DISPLAY_NAME + " ASC",
+                            )
 
-                    cursor?.let { csr ->
-                        if (csr.count > 0) {
-                            while (csr.moveToNext()) {
-                                val id =
-                                    csr.getLong(csr.getColumnIndexOrThrow(ContactsContract.Contacts._ID))
-                                val name =
-                                    csr.getString(csr.getColumnIndexOrThrow(ContactsContract.Contacts.DISPLAY_NAME))
-                                val hasPhoneNumber =
-                                    csr.getInt(csr.getColumnIndexOrThrow(ContactsContract.Contacts.HAS_PHONE_NUMBER))
-                                if (hasPhoneNumber > 0) {
-                                    val phoneCursor =
-                                        contentResolver.query(
-                                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                                            null,
-                                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
-                                            arrayOf(id.toString()),
-                                            null,
-                                        )
+                        cursor?.let { csr ->
+                            if (csr.count > 0) {
+                                while (csr.moveToNext()) {
+                                    val id =
+                                        csr.getLong(csr.getColumnIndexOrThrow(ContactsContract.Contacts._ID))
+                                    val name =
+                                        csr.getString(csr.getColumnIndexOrThrow(ContactsContract.Contacts.DISPLAY_NAME))
+                                    val hasPhoneNumber =
+                                        csr.getInt(csr.getColumnIndexOrThrow(ContactsContract.Contacts.HAS_PHONE_NUMBER))
+                                    if (hasPhoneNumber > 0) {
+                                        val phoneCursor =
+                                            contentResolver.query(
+                                                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                                                null,
+                                                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                                                arrayOf(id.toString()),
+                                                null,
+                                            )
 
-                                    phoneCursor?.let { phoneCsr ->
-                                        if (phoneCsr.count > 0) {
-                                            while (phoneCsr.moveToNext()) {
-                                                val number =
-                                                    phoneCsr.getString(
-                                                        phoneCsr.getColumnIndexOrThrow(
-                                                            ContactsContract.CommonDataKinds.Phone.NUMBER,
-                                                        ),
-                                                    )
-                                                contactList.add(Contact(id.toInt(), name, number))
+                                        phoneCursor?.let { phoneCsr ->
+                                            if (phoneCsr.count > 0) {
+                                                while (phoneCsr.moveToNext()) {
+                                                    val number =
+                                                        phoneCsr.getString(
+                                                            phoneCsr.getColumnIndexOrThrow(
+                                                                ContactsContract.CommonDataKinds.Phone.NUMBER,
+                                                            ),
+                                                        )
+                                                    contactList.add(Contact(id, name, number))
+                                                }
                                             }
                                         }
+                                        phoneCursor?.close()
                                     }
-                                    phoneCursor?.close()
                                 }
                             }
                         }
+                        cursor?.close()
+                    } catch (illegalArgumentException: IllegalArgumentException) {
+                        Log.e(TAG, "illegalArgumentException: $illegalArgumentException")
+                    } catch (exception: Exception) {
+                        Log.e(TAG, "exception: $exception")
                     }
-                    cursor?.close()
-                } catch (illegalArgumentException: IllegalArgumentException) {
-                    Log.e(TAG, "illegalArgumentException: $illegalArgumentException")
-                } catch (exception: Exception) {
-                    Log.e(TAG, "exception: $exception")
                 }
+                cache.addAll(contactList.distinctBy { it.id }.toSet())
+                cache
             }
-            return contactList.distinctBy { it.id }.toSet()
-        }
-
-        companion object {
-            private const val TAG = "ContactFetcherImpl"
+        } finally {
+            cacheMutex.unlock()
         }
     }
+
+    fun clearCache() {
+        cache.clear()
+    }
+
+    companion object {
+        private const val TAG = "ContactFetcherImpl"
+    }
+}
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -129,7 +149,7 @@ object DatabaseModule {
     @Singleton
     fun providesContactFetcher(
         @ApplicationContext context: Context,
-    ): ContactFetcherImpl = ContactFetcherImpl(context)
+    ): ContactFetcher = ContactFetcherImpl(context)
 }
 
 tailrec fun Context?.activity(): Activity? =
